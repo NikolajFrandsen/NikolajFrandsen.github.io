44420893745b1023a5032c651a5458b9ca199cfe
Nikolaj Frandsen <nifr15ab@student.cbs.dk>
2018-09-24 10:54:09 +0200

    First

---
+++ apps.js
@@ -0,0 +1,34 @@
+var submit = document.getElementById('submit');
+
+var objPeople = [
+    {
+        username: "Frederik",
+        password: "123"
+    },
+    {
+        username: "Martin",
+        password: "1234"
+    },
+    {
+        username: "Nikolaj",
+        password: "12345"
+    }
+]
+
+function getInfo(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    for (i = 0; i < objPeople.length; i++){
+        if(username == objPeople[i].username && password == objPeople[i].password){
+            alert("Login successful");
+            return true;
+        }
+    }
+    alert("Wrong username and/or password")   
+    console.log("Button is clicked");
+};
+
+
+
+  
+
