cc856700ee990ce28783cf7ec6c06ae0bab1afaf
Nikolaj Frandsen <nifr15ab@student.cbs.dk>
2018-10-17 15:00:30 +0200

    Minor tests.

--- scripts/createuser.js
+++ scripts/createuser.js
@@ -1,49 +1,88 @@
-// CREATE NEW USER
+// // CREATE NEW USER
 
-function registerUser(){
+// function registerUser(){
 
-    //Retrieve information from input fields
+//     //Retrieve information from input fields
 
-    var registerUser = document.getElementById("newUser").value
-    var registerPassword = document.getElementById("newPassword").value
+//     var registerUser = document.getElementById("newUser").value
+//     var registerPassword = document.getElementById("newPassword").value
     
-    //Create new variable which we can push to our object. 
+//     //Create new variable which we can push to our object. 
     
-    var newUser = {
-        username: registerUser,
-        password: registerPassword
+//     var newUser = {
+//         username: registerUser,
+//         password: registerPassword
+//     }
+
+//     // Make sure the username is not taken. 
+
+//     for (i = 0; i < objPeople.length;i++){
+//         if(registerUser == objPeople[i].username){
+//             alert("that username is already in use")
+//             return
+
+//     //Check password length
+
+//     } else if (registerPassword.length < 8) {
+//             alert("That password is too short")
+//             return
+//         }
+//     }
+
+//     // Push new user to our object in the top.  
+
+//     objPeople.push(newUser)
+//     console.log(objPeople)
+// }
+
+// var addToLocalStorageObject = function (objPeople, newUser, value){
+//     var existing = localStorage.getItem(objPeople);
+//     existing = existing ? JSON.parse(existing) : {};
+//     existing[newUser] = value; 
+//     localStorage.setItem(objPeople, JSON.stringify(existing));
+
+// };
+
+// // var existing = localStorage.getItem(objPeople);
+// // existing = existing ? JSON.parse(existing):{};
+// // existing['username'] = newUser;
+// // localStorage.setItem('objPeople', JSON.stringify(existing));
+
+
+
+
+// var txt = '{"spot":"Klitmoeller", "country":"Denmark"}'
+var txt = [
+    {
+        spot: "klitmoeller",
+        country: 'Denmark'
+    },
+    {
+        spot: "lofoten",
+        country: "Norway"
     }
+]
 
-    // Make sure the username is not taken. 
+let txtString = JSON.stringify(txt)
+localStorage.setItem('bucketItem', txtString)
 
-    for (i = 0; i < objPeople.length;i++){
-        if(registerUser == objPeople[i].username){
-            alert("that username is already in use")
-            return
+let newItem = localStorage.getItem('bucketItem')
+console.log(JSON.parse(newItem))
+console.log(localStorage.getItem(newItem))
+document.getElementById("bucketlisttest").innerHTML = newItem.spot;
 
-    //Check password length
+// var obj = JSON.parse(txt);
+// document.getElementById("bucketlisttest").innerHTML = obj.spot + ", " + obj.country;
 
-    } else if (registerPassword.length < 8) {
-            alert("That password is too short")
-            return
-        }
-    }
-
-    // Push new user to our object in the top.  
-
-    objPeople.push(newUser)
-    console.log(objPeople)
-}
-
-var addToLocalStorageObject = function (objPeople, newUser, value){
-    var existing = localStorage.getItem(objPeople);
-    existing = existing ? JSON.parse(existing) : {};
-    existing[newUser] = value; 
-    localStorage.setItem(objPeople, JSON.stringify(existing));
-
-};
-
-// var existing = localStorage.getItem(objPeople);
-// existing = existing ? JSON.parse(existing):{};
-// existing['username'] = newUser;
-// localStorage.setItem('objPeople', JSON.stringify(existing));
+// var json = emptyBucket, 
+// data = JSON.parse("json", function(key, value){
+//     if (key !== "" && typeof value == 'object'){
+//         Object.keys.foreach(function(name){
+//             if (name.slice(-1) !== '2'){
+//                 delete vale[name];
+//             }
+//         });
+//     }
+//     return value;
+// });
+// document.body.appendChild(document.createTextNode(JSON.stringify(data)));
